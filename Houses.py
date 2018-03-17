e19f946411b81fb7ec48f3cb87798f0cdc818a44
Emohamed <emad.ezzeldin4@gmail.com>
2018-03-17 09:03:07 -0400

    TrimFunction
    I put all the lines used to trim the data frame to 2000-2016 in one
    function.

--- Houses.py
+++ Houses.py
@@ -6,18 +6,16 @@ path      = '/Users/emadezzeldin/Emad Dropbox/Emad Mohamed/Automating_Applying/c
 filename  = 'City_Zhvi_AllHomes.csv'
 housesfile= path + filename
 houses    = pd.read_csv (housesfile)
-#print (houses)
-columns = houses.columns
-numcol  = columns [8:]
-#print (numcol)
-def get2000 (column):
-    strcolumn = str (column)
-    year      = strcolumn.split('-')[0]
-    return int(year) >= 2000 and int(year) <= 2016
-wantedcol= [col for col in numcol if get2000(col)]
-#print (wantedcol)
-#wantedcol.append ('State')
-#wantedcol.append ('RegionName')
-wantedcol = wantedcol + ['RegionID','RegionName'	,'State'	,'Metro'	,'CountyName'	,'SizeRank']
-c.WantedCols (houses,wantedcol)
+def trimdf (houses):
+    columns = houses.columns
+    numcol  = columns [8:]
+    def get2000 (column):
+        strcolumn = str (column)
+        year      = strcolumn.split('-')[0]
+        return int(year) >= 2000 and int(year) <= 2016
+    wantedcol= [col for col in numcol if get2000(col)]
+    wantedcol = wantedcol + ['RegionID','RegionName'	,'State'	,'Metro'	,'CountyName'	,'SizeRank']
+    c.WantedCols (houses,wantedcol)
+    return houses
+houses    = trimdf(houses)
 print (houses)
